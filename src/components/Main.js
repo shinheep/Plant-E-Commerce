f367d001a495d73ce970415a65e3002ead819e60
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-12-15 20:45:09 -0800

    Install react-scroll, and created functional button to scroll to plant component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,11 @@
+import {Link} from 'react-scroll'
+
 const Main = () => {
     return (
         <div className="main">
             <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_ca0xjdgv.json"  background="transparent"  speed="1"  style={{width: "300px"}, {height: "300px"}} autoplay></lottie-player>
             <div className="title">Plantify</div>
+            <Link activeClass="active" to="plant" spy={true} smooth={false}><button>Shop Plants</button></Link>
         </div>
     )
 }
