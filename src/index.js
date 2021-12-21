d983699194e72de5627e1278cacfff4544f205ed
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-12-21 09:16:56 -0800

    Changed homepage link

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
