765836b7d8e95100f46f4f41dbabf607ef1557c9
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-12-21 09:28:09 -0800

    changed to hashrouter

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <HashRouter>
         <App />
-    </Router>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
